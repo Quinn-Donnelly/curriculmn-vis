27ab98334cf29c58318b117b014238f8a5ca07c1
Quintin Donnelly <quintin.donnelly@revature.com>
2020-01-03 17:44:00 -0500

    WIP: Updated the data strcture need to make code mod moving comp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import List from '@material-ui/core/List';
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Chip from "@material-ui/core/Chip";
+import Grid from '@material-ui/core/Grid';
+import Divider from "@material-ui/core/Divider";
+import Paper from "@material-ui/core/Paper";
+import curriculums from './data/curriculum';
+
+let skills = new Set();
+let curriculum = Object.keys(curriculums);
+curriculum.sort();
+let skillMap = new Map();
+for (let entry of Object.entries(curriculums)) {
+  entry[1].forEach((skill) => {
+    skills.add(skill);
+  });
+  skillMap.set(entry[0], new Set(entry[1]));
+}
 
 function App() {
+  let [selectedList, setSelectedList] = useState();
+  let [currentSkillSet, updateCurrentSkillSet] = useState(skills);
+
+  function switchActive(event) {
+    console.log(event.currentTarget.id);
+    setSelectedList(event.currentTarget.id);
+    updateCurrentSkillSet(skillMap.get(event.currentTarget.id));
+  }
+
+  let listItems = [];
+  curriculum.forEach((name) => listItems.push(
+      <Paper>
+      <ListItem button key={name} id={name} onMouseOver={switchActive} selected={selectedList === name}>
+          <ListItemText primary={name} />
+      </ListItem></Paper>
+  ));
+
+  let skillChips = [];
+  skills.forEach((skill) => {
+    let hideClass = currentSkillSet.has(skill) ? "" : "fade";
+    skillChips.push(
+        <Chip component="button" label={skill} key={skill} className={hideClass + " chip"}/>
+    )
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <List component="nav">
+            {listItems}
+          </List>
+        </Grid>
+        <Grid item xs>
+          {skillChips}
+        </Grid>
+      </Grid>
     </div>
   );
 }
---
+++ src/data/curriculum.js
@@ -0,0 +1,220 @@
+let json = {
+    "Java Full Stack": [
+        {
+            name: "Angular",
+            type: "framework"
+        },
+        {
+            name: "Bootstrap",
+            type: "framework"
+        },
+        {
+            name: "Spring Boot",
+            type: "framework"
+        },
+        {
+            name: "Jenkins",
+            type: "devops"
+        },
+        {
+            name: "Maven",
+            type: "devops"
+        },
+        {
+            name: "npm",
+            type: "devops"
+        },
+        {
+            name: "MVC",
+            type: "architecture"
+        },
+
+        {
+            name: "Cloud Fundamentals",
+            type: "architecture"
+        },
+        {
+            name: "SOA",
+            type: "architecture"
+        },
+        {
+            name: "Microservices",
+            type: "architecture"
+        },
+        {
+            name: "Containers",
+            type: "architecture"
+        },
+        {
+            name: "RDBMS",
+            type: "database"
+        },
+        {
+            name: "RDS",
+            type: "database"
+        },
+        {
+            name: "Github",
+            type: "sourcecode"
+        },
+        {
+            name: "git",
+            type: "sourcecode"
+        },
+        {
+            name: "Postman",
+            type: "ide"
+        },
+        {
+            name: "Visual Studio Code",
+            type: "ide"
+        },
+        {
+            name: "Intellij",
+            type: "ide"
+        },
+        {
+            name: "Eclipse",
+            type: "ide"
+        }
+    ],
+    "Java with Test Automation": [
+        {
+            name: "Angular",
+            type: "framework"
+        },
+        {
+            name: "Bootstrap",
+            type: "framework"
+        },
+        {
+            name: "Spring Boot",
+            type: "framework"
+        },
+        {
+            name: "Jenkins",
+            type: "devops"
+        },
+        {
+            name: "Maven",
+            type: "devops"
+        },
+        {
+            name: "npm",
+            type: "devops"
+        },
+        {
+            name: "MVC",
+            type: "architecture"
+        },
+
+        {
+            name: "Cloud Fundamentals",
+            type: "architecture"
+        },
+        {
+            name: "SOA",
+            type: "architecture"
+        },
+        {
+            name: "RDBMS",
+            type: "database"
+        },
+        {
+            name: "RDS",
+            type: "database"
+        },
+        {
+            name: "Github",
+            type: "sourcecode"
+        },
+        {
+            name: "git",
+            type: "sourcecode"
+        },
+        {
+            name: "Postman",
+            type: "ide"
+        },
+        {
+            name: "Visual Studio Code",
+            type: "ide"
+        },
+        {
+            name: "Intellij",
+            type: "ide"
+        },
+        {
+            name: "Eclipse",
+            type: "ide"
+        },
+        {
+            name: "TestNG",
+            type: "framework"
+        },
+        {
+            name: "Selenium",
+            type: "framework"
+        }
+    ],
+    ".NET Full Stack": [
+        {
+            name: "Entity Framework",
+            type: "framework"
+        },
+        {
+            name: "ASP .NET",
+            type: "framework"
+        },
+        {
+            name: "Angular",
+            type: "framework"
+        },
+        {
+            name: "npm",
+            type: "devops"
+        },
+        {
+            name: "Azure",
+            type: "devops"
+        },
+        {
+            name: "docker",
+            type: "devops"
+        },
+        {
+            name: "SOA",
+            type: "architecture"
+        },
+        {
+            name: "Microservices",
+            type: "architecture"
+        },
+        {
+            name: "MVC",
+            type: "architecture"
+        },
+        {
+            name: "Cloud Fundamentals",
+            type: "architecture"
+        },
+        {
+            name: "git",
+            type: "sourcecode"
+        },
+        {
+            name: "GitHub",
+            type: "sourcecode"
+        },
+        {
+            name: "Visual Studio Code",
+            type: "ide"
+        },
+        {
+            name: "Visual Studio",
+            type: "ide"
+        }
+    ]
+};
+
+export default json;
